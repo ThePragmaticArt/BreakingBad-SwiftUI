2c8fdba6001ac566b25ad599632bc0652fd60de3
Brandon Levasseur <brandon_levasseur@papajohns.com>
2021-10-11 11:05:23 -0400

    Initial Commit

---
+++ BreakingBad/BreakingBadApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakingBadApp.swift
+//  BreakingBad
+//
+//  Created by Brandon Levasseur on 10/11/21.
+//
+
+import SwiftUI
+
+@main
+struct BreakingBadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
